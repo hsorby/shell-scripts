34f4b563a0cd69a3057a361736c2a10a43af1705
Hugh Sorby <h.sorby@auckland.ac.nz>
2022-01-31 23:10:00 +1300

    Add script for calculating the total number of tests run with CTest.

---
+++ libcellml/calculate_test_total.py
@@ -0,0 +1,24 @@
+
+#!/usr/bin/env python
+
+import re
+import sys
+
+
+def main():
+    captured_output = sys.argv[1]
+    with open(captured_output) as f:
+        lines = f.readlines()
+
+    tot = 0
+    for line in lines:
+        a = re.search("^[\d]+: \[==========\] ([\d]+) tests from ([\d]+) test case[s]? ran.*", line)
+        if a is not None:
+            tot += int(a.group(1))
+
+    print("Total test count: ", tot)
+
+
+if __name__ == "__main__":
+    main()
+
